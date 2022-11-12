bb75118ffb8646bd18075a970a14c18cdb3db328
HelloprogramingHanif <114008336+HelloprogramingHanif@users.noreply.github.com>
2022-11-12 12:01:40 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,23 @@
+//Falsy
+
+// 0
+//""
+// undenfined
+// null
+// NaN
+
+//const name = "0"
+
+//truthy
+
+let name = [];
+//console.log(name);
+
+ if(name){
+    console.log("condition is true");
+ }
+
+ else{
+    console.log("condition is false");
+ }
+     
